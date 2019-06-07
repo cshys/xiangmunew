3f5e9ea668d91bc7b75431bd7e08dfa4d61c5507
cshys <1351548326@qq.com>
2019-06-07 22:10:08 +0800

    2019-6-7

---
+++ xiangmu/register/js/header.js
@@ -0,0 +1,7 @@
+﻿document.writeln("<div id=\'header\'>");
+document.writeln("        <div class=\'margin\'>");
+document.writeln("            <a href=\'../index.html\'>");
+document.writeln("                <img src=\'img/logo_new.jpg\' alt=\'\'>");
+document.writeln("            </a>");
+document.writeln("        </div>");
+document.writeln("    </div>");
